c4e4fffb4206e0b5829164f7ec8659464093b393
Insidiae <insidiae423@gmail.com>
2018-06-15 16:25:46 +0800

    Finish Section 5 (Completed Natours project for desktop view)

---
+++ Natours/js/nav.js
@@ -0,0 +1,19 @@
+const body = document.querySelector("body");
+const menuCheckbox = document.querySelector(".navigation__checkbox");
+const links = document.querySelectorAll(".navigation__link");
+
+menuCheckbox.addEventListener("change", function() {
+  console.log(this);
+  if (this.checked) {
+    body.classList.add("u-hide-overflow-y");
+  } else {
+    body.classList.remove("u-hide-overflow-y");
+  }
+});
+
+links.forEach(link =>
+  link.addEventListener("click", () => {
+    body.classList.remove("u-hide-overflow-y");
+    menuCheckbox.checked = false;
+  })
+);
