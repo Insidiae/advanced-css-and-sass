2a5949cc42f4cf5668f24c81d7f77b50e4ee1a7d
Kobe Ruado <insidiae423@gmail.com>
2022-04-01 00:51:28 +0800

    Add smooth scroll + sticky header

---
+++ Omnifood/js/script.js
@@ -0,0 +1,76 @@
+// Prevent transitions from firing upon page load
+// https://css-tricks.com/transitions-only-after-page-load/
+function ready(fn) {
+  if (document.readyState != "loading") {
+    fn();
+  } else {
+    document.addEventListener("DOMContentLoaded", fn);
+  }
+}
+
+ready(() => document.body.classList.remove("preload"));
+
+// Set current year on footer
+const yearElement = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+yearElement.textContent = currentYear;
+
+// Make mobile navigation work
+const navBtn = document.querySelector(".mobile-nav__btn");
+const header = document.querySelector(".header");
+
+navBtn.addEventListener("click", () => {
+  header.classList.toggle("nav-open");
+});
+
+// Smooth scrolling polyfill
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile naviagtion
+    if (link.classList.contains("main-nav__link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
+
+// Sticky navigation
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
